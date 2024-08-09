ab0484673e8f2205a20afb416ae2b0e08aa1ca67
Maxence Depire <56301052+maxence-depire@users.noreply.github.com>
2024-08-10 01:28:06 +0200

    first commit

---
+++ main.pl
@@ -0,0 +1,13 @@
+region(auvergneRhôneAlpes).
+region(bourgogneFrancheComte).
+region(bretagne).
+region(centreValDeLoire).
+region(corse).
+region(grandEst).
+region(hautsDeFrance).
+region(ileDeFrance).
+region(normandie).
+region(nouvelleAquitaine).
+region(occitanie).
+region(paysDeLaLoire).
+region(provenceAlpesCoteDAzur).
